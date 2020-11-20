32ab4a782775fb5b42c83599215b81e747785b5f
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-11-20 15:03:04 -0600

    install react dom

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles.css';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ 
+class App extends React Component {
+  state = {
+    color: "blue"
+  }
+  onChangeHandler = e => {
+    this.setState({
+      color: e.target.value
+    })
+  }
+  render(){
+    const styleObj = {
+      background = this.state.color
+    }
+    return (
+      <div style={styleObj} className="App">
+        <input 
+          onChange={this.onChangeHandler}
+          value={this.state.color}
+          type="text" /> 
+      </div>
+    )
+  }
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
